b3c3b12878ec76e6c83391f76ba094770ed21580
Jorge <jorgeferram17@gatech.edu>
2022-08-05 17:55:08 -0400

    minot fix

--- lua/jlf/remap.lua
+++ lua/jlf/remap.lua
@@ -7,3 +7,6 @@ nnoremap("<C-J>", "<C-W><C-J>")
 nnoremap("<C-K>", "<C-W><C-K>") 
 nnoremap("<C-L>", "<C-W><C-L>") 
 nnoremap("<C-H>", "<C-W><C-H>") 
+
+-- remap for vim fugitive
+nnoremap("<leader>gs", "<cmd>G<CR>") 
